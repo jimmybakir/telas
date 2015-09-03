45a1a4112e181e5319d5c4c5ea7317dede854e41
Sergio <sergio.hinojosa.avila@gmail.com>
2015-09-03 02:49:25 -0400

    Vista : Crear Sub Procesos : Cuba Principal
    Se agregaron todas las funciones disponibles en el PLC cada funcion
    puede tener parametros como nivel, temperatura y tiempos.

--- ScadaTelas/FormCrearProcesos.Designer.cs
+++ ScadaTelas/FormCrearProcesos.Designer.cs
@@ -28,20 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gCubaPrincipal = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.CPLlenarNivel = new System.Windows.Forms.CheckBox();
+            this.CPLlenarTiempo = new System.Windows.Forms.CheckBox();
+            this.CPVaciar = new System.Windows.Forms.CheckBox();
+            this.CPEnfriar = new System.Windows.Forms.CheckBox();
+            this.CPCalentar = new System.Windows.Forms.CheckBox();
+            this.CPLlenar = new System.Windows.Forms.CheckBox();
+            this.CubaPrincipal = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.CPCalentarTemp = new System.Windows.Forms.CheckBox();
+            this.CPCaletarTiempo = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.CPEnfriarTemp = new System.Windows.Forms.CheckBox();
+            this.CPEnfriarTiempo = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.CPVaciarTemp = new System.Windows.Forms.CheckBox();
+            this.CPVaciarTiempo = new System.Windows.Forms.CheckBox();
+            this.gCubaPrincipal.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gCubaPrincipal
+            // 
+            this.gCubaPrincipal.Controls.Add(this.flowLayoutPanel4);
+            this.gCubaPrincipal.Controls.Add(this.flowLayoutPanel1);
+            this.gCubaPrincipal.Controls.Add(this.CPVaciar);
+            this.gCubaPrincipal.Controls.Add(this.flowLayoutPanel3);
+            this.gCubaPrincipal.Controls.Add(this.flowLayoutPanel2);
+            this.gCubaPrincipal.Controls.Add(this.CPEnfriar);
+            this.gCubaPrincipal.Controls.Add(this.CPCalentar);
+            this.gCubaPrincipal.Controls.Add(this.CPLlenar);
+            this.gCubaPrincipal.Location = new System.Drawing.Point(14, 50);
+            this.gCubaPrincipal.Name = "gCubaPrincipal";
+            this.gCubaPrincipal.Size = new System.Drawing.Size(721, 135);
+            this.gCubaPrincipal.TabIndex = 0;
+            this.gCubaPrincipal.TabStop = false;
+            this.gCubaPrincipal.Text = "Cuba Principal";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.CPLlenarNivel);
+            this.flowLayoutPanel1.Controls.Add(this.CPLlenarTiempo);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(60, 56);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(110, 52);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // CPLlenarNivel
+            // 
+            this.CPLlenarNivel.AutoSize = true;
+            this.CPLlenarNivel.Location = new System.Drawing.Point(3, 3);
+            this.CPLlenarNivel.Name = "CPLlenarNivel";
+            this.CPLlenarNivel.Size = new System.Drawing.Size(53, 19);
+            this.CPLlenarNivel.TabIndex = 0;
+            this.CPLlenarNivel.Text = "Nivel";
+            this.CPLlenarNivel.UseVisualStyleBackColor = true;
+            // 
+            // CPLlenarTiempo
+            // 
+            this.CPLlenarTiempo.AutoSize = true;
+            this.CPLlenarTiempo.Location = new System.Drawing.Point(3, 28);
+            this.CPLlenarTiempo.Name = "CPLlenarTiempo";
+            this.CPLlenarTiempo.Size = new System.Drawing.Size(84, 19);
+            this.CPLlenarTiempo.TabIndex = 1;
+            this.CPLlenarTiempo.Text = "Tiempo [s]";
+            this.CPLlenarTiempo.UseVisualStyleBackColor = true;
+            // 
+            // CPVaciar
+            // 
+            this.CPVaciar.AutoSize = true;
+            this.CPVaciar.Location = new System.Drawing.Point(543, 31);
+            this.CPVaciar.Name = "CPVaciar";
+            this.CPVaciar.Size = new System.Drawing.Size(59, 19);
+            this.CPVaciar.TabIndex = 3;
+            this.CPVaciar.Text = "Vaciar";
+            this.CPVaciar.UseVisualStyleBackColor = true;
+            // 
+            // CPEnfriar
+            // 
+            this.CPEnfriar.AutoSize = true;
+            this.CPEnfriar.Location = new System.Drawing.Point(378, 31);
+            this.CPEnfriar.Name = "CPEnfriar";
+            this.CPEnfriar.Size = new System.Drawing.Size(62, 19);
+            this.CPEnfriar.TabIndex = 2;
+            this.CPEnfriar.Text = "Enfriar";
+            this.CPEnfriar.UseVisualStyleBackColor = true;
+            // 
+            // CPCalentar
+            // 
+            this.CPCalentar.AutoSize = true;
+            this.CPCalentar.Location = new System.Drawing.Point(213, 31);
+            this.CPCalentar.Name = "CPCalentar";
+            this.CPCalentar.Size = new System.Drawing.Size(73, 19);
+            this.CPCalentar.TabIndex = 1;
+            this.CPCalentar.Text = "Calentar";
+            this.CPCalentar.UseVisualStyleBackColor = true;
+            // 
+            // CPLlenar
+            // 
+            this.CPLlenar.AutoSize = true;
+            this.CPLlenar.Location = new System.Drawing.Point(43, 31);
+            this.CPLlenar.Name = "CPLlenar";
+            this.CPLlenar.Size = new System.Drawing.Size(61, 19);
+            this.CPLlenar.TabIndex = 0;
+            this.CPLlenar.Text = "Llenar";
+            this.CPLlenar.UseVisualStyleBackColor = true;
+            // 
+            // CubaPrincipal
+            // 
+            this.CubaPrincipal.AutoSize = true;
+            this.CubaPrincipal.Location = new System.Drawing.Point(14, 27);
+            this.CubaPrincipal.Name = "CubaPrincipal";
+            this.CubaPrincipal.Size = new System.Drawing.Size(107, 19);
+            this.CubaPrincipal.TabIndex = 1;
+            this.CubaPrincipal.Text = "Cuba Principal";
+            this.CubaPrincipal.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.CPCalentarTemp);
+            this.flowLayoutPanel2.Controls.Add(this.CPCaletarTiempo);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(233, 56);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(121, 52);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // CPCalentarTemp
+            // 
+            this.CPCalentarTemp.AutoSize = true;
+            this.CPCalentarTemp.Location = new System.Drawing.Point(3, 3);
+            this.CPCalentarTemp.Name = "CPCalentarTemp";
+            this.CPCalentarTemp.Size = new System.Drawing.Size(108, 19);
+            this.CPCalentarTemp.TabIndex = 0;
+            this.CPCalentarTemp.Text = "Temperatura C";
+            this.CPCalentarTemp.UseVisualStyleBackColor = true;
+            // 
+            // CPCaletarTiempo
+            // 
+            this.CPCaletarTiempo.AutoSize = true;
+            this.CPCaletarTiempo.Location = new System.Drawing.Point(3, 28);
+            this.CPCaletarTiempo.Name = "CPCaletarTiempo";
+            this.CPCaletarTiempo.Size = new System.Drawing.Size(84, 19);
+            this.CPCaletarTiempo.TabIndex = 1;
+            this.CPCaletarTiempo.Text = "Tiempo [s]";
+            this.CPCaletarTiempo.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.CPEnfriarTemp);
+            this.flowLayoutPanel3.Controls.Add(this.CPEnfriarTiempo);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(397, 56);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(121, 52);
+            this.flowLayoutPanel3.TabIndex = 6;
+            // 
+            // CPEnfriarTemp
+            // 
+            this.CPEnfriarTemp.AutoSize = true;
+            this.CPEnfriarTemp.Location = new System.Drawing.Point(3, 3);
+            this.CPEnfriarTemp.Name = "CPEnfriarTemp";
+            this.CPEnfriarTemp.Size = new System.Drawing.Size(108, 19);
+            this.CPEnfriarTemp.TabIndex = 0;
+            this.CPEnfriarTemp.Text = "Temperatura C";
+            this.CPEnfriarTemp.UseVisualStyleBackColor = true;
+            // 
+            // CPEnfriarTiempo
+            // 
+            this.CPEnfriarTiempo.AutoSize = true;
+            this.CPEnfriarTiempo.Location = new System.Drawing.Point(3, 28);
+            this.CPEnfriarTiempo.Name = "CPEnfriarTiempo";
+            this.CPEnfriarTiempo.Size = new System.Drawing.Size(84, 19);
+            this.CPEnfriarTiempo.TabIndex = 1;
+            this.CPEnfriarTiempo.Text = "Tiempo [s]";
+            this.CPEnfriarTiempo.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Controls.Add(this.CPVaciarTemp);
+            this.flowLayoutPanel4.Controls.Add(this.CPVaciarTiempo);
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(562, 56);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(121, 52);
+            this.flowLayoutPanel4.TabIndex = 7;
+            // 
+            // CPVaciarTemp
+            // 
+            this.CPVaciarTemp.AutoSize = true;
+            this.CPVaciarTemp.Location = new System.Drawing.Point(3, 3);
+            this.CPVaciarTemp.Name = "CPVaciarTemp";
+            this.CPVaciarTemp.Size = new System.Drawing.Size(108, 19);
+            this.CPVaciarTemp.TabIndex = 0;
+            this.CPVaciarTemp.Text = "Temperatura C";
+            this.CPVaciarTemp.UseVisualStyleBackColor = true;
+            // 
+            // CPVaciarTiempo
+            // 
+            this.CPVaciarTiempo.AutoSize = true;
+            this.CPVaciarTiempo.Location = new System.Drawing.Point(3, 28);
+            this.CPVaciarTiempo.Name = "CPVaciarTiempo";
+            this.CPVaciarTiempo.Size = new System.Drawing.Size(84, 19);
+            this.CPVaciarTiempo.TabIndex = 1;
+            this.CPVaciarTiempo.Text = "Tiempo [s]";
+            this.CPVaciarTiempo.UseVisualStyleBackColor = true;
             // 
             // FormCrearProcesos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(691, 382);
+            this.ClientSize = new System.Drawing.Size(806, 441);
+            this.Controls.Add(this.CubaPrincipal);
+            this.Controls.Add(this.gCubaPrincipal);
+            this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FormCrearProcesos";
             this.Text = "ScadaTelas :: Crear Procesos";
+            this.gCubaPrincipal.ResumeLayout(false);
+            this.gCubaPrincipal.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gCubaPrincipal;
+        private System.Windows.Forms.CheckBox CubaPrincipal;
+        private System.Windows.Forms.CheckBox CPVaciar;
+        private System.Windows.Forms.CheckBox CPEnfriar;
+        private System.Windows.Forms.CheckBox CPCalentar;
+        private System.Windows.Forms.CheckBox CPLlenar;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.CheckBox CPLlenarNivel;
+        private System.Windows.Forms.CheckBox CPLlenarTiempo;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.CheckBox CPVaciarTemp;
+        private System.Windows.Forms.CheckBox CPVaciarTiempo;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.CheckBox CPEnfriarTemp;
+        private System.Windows.Forms.CheckBox CPEnfriarTiempo;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.CheckBox CPCalentarTemp;
+        private System.Windows.Forms.CheckBox CPCaletarTiempo;
     }
 }
 
