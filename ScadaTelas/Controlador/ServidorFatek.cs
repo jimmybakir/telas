bbb3e77b31cfde3f4b418fc32a81f4c575526fd0
Sergio <sergio.hinojosa.avila@gmail.com>
2015-09-07 14:23:50 -0400

    Controlador : Creacion ServidorFatek

---
+++ ScadaTelas/Controlador/ServidorFatek.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FaconSvr;
+
+namespace ScadaTelas.Controlador
+{
+    class ServidorFatek : FaconSvr.FaconServer 
+    {
+        protected ServidorFatek()
+        {
+        }
+        protected ServidorFatek(string p): this()
+        {
+        }
+        internal static FaconServer CreateObject(string p)
+        {
+            return new FaconServer(p);
+        }
+
+        internal void OpenProject(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string GetItem(string p, string p_2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
