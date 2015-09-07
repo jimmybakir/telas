0bd16eab354dc6a8234d68a7cf78ab389510b5f0
Sergio <sergio.hinojosa.avila@gmail.com>
2015-09-07 14:08:59 -0400

    Vista : Crear Proceso con Receta

---
+++ ScadaTelas/CrearRecetaProceso.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ScadaTelas
+{
+    partial class CrearRecetaProceso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.no = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.subProceso = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.param1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.param2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.param3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.param4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.bAgregar = new System.Windows.Forms.Button();
+            this.bSubir = new System.Windows.Forms.Button();
+            this.bBajar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(652, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(187, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.no,
+            this.subProceso,
+            this.param1,
+            this.param2,
+            this.param3,
+            this.param4});
+            this.listView1.Location = new System.Drawing.Point(12, 32);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(482, 277);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // no
+            // 
+            this.no.Text = "No";
+            this.no.Width = 38;
+            // 
+            // subProceso
+            // 
+            this.subProceso.Text = "Sub Proceso";
+            this.subProceso.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.subProceso.Width = 100;
+            // 
+            // param1
+            // 
+            this.param1.Text = "Param 1";
+            this.param1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.param1.Width = 78;
+            // 
+            // param2
+            // 
+            this.param2.Text = "Param 2";
+            this.param2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.param2.Width = 77;
+            // 
+            // param3
+            // 
+            this.param3.Text = "Param 3";
+            this.param3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.param3.Width = 77;
+            // 
+            // param4
+            // 
+            this.param4.Text = "Param 4";
+            this.param4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.param4.Width = 77;
+            // 
+            // bAgregar
+            // 
+            this.bAgregar.Location = new System.Drawing.Point(533, 130);
+            this.bAgregar.Name = "bAgregar";
+            this.bAgregar.Size = new System.Drawing.Size(75, 23);
+            this.bAgregar.TabIndex = 2;
+            this.bAgregar.Text = "<<";
+            this.bAgregar.UseVisualStyleBackColor = true;
+            // 
+            // bSubir
+            // 
+            this.bSubir.Location = new System.Drawing.Point(533, 159);
+            this.bSubir.Name = "bSubir";
+            this.bSubir.Size = new System.Drawing.Size(75, 23);
+            this.bSubir.TabIndex = 3;
+            this.bSubir.Text = "Subir";
+            this.bSubir.UseVisualStyleBackColor = true;
+            // 
+            // bBajar
+            // 
+            this.bBajar.Location = new System.Drawing.Point(533, 189);
+            this.bBajar.Name = "bBajar";
+            this.bBajar.Size = new System.Drawing.Size(75, 23);
+            this.bBajar.TabIndex = 4;
+            this.bBajar.Text = "Bajar";
+            this.bBajar.UseVisualStyleBackColor = true;
+            // 
+            // CrearRecetaProceso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 321);
+            this.Controls.Add(this.bBajar);
+            this.Controls.Add(this.bSubir);
+            this.Controls.Add(this.bAgregar);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "CrearRecetaProceso";
+            this.Text = "CrearRecetaProceso";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader no;
+        private System.Windows.Forms.ColumnHeader subProceso;
+        private System.Windows.Forms.ColumnHeader param1;
+        private System.Windows.Forms.ColumnHeader param2;
+        private System.Windows.Forms.ColumnHeader param3;
+        private System.Windows.Forms.ColumnHeader param4;
+        private System.Windows.Forms.Button bAgregar;
+        private System.Windows.Forms.Button bSubir;
+        private System.Windows.Forms.Button bBajar;
+    }
+}
--- ScadaTelas/FormCrearProcesos2.Designer.cs
+++ ScadaTelas/FormCrearProcesos2.Designer.cs
@@ -32,11 +32,13 @@
             this.lbCodigoProceso = new System.Windows.Forms.Label();
             this.lbNombreProceso = new System.Windows.Forms.Label();
             this.txNombreProceso = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.No = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SubProceso = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Param1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Param2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Param3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Param4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // txCodigoProceso
@@ -73,46 +75,68 @@
             this.txNombreProceso.Size = new System.Drawing.Size(154, 20);
             this.txNombreProceso.TabIndex = 4;
             // 
-            // dataGridView1
+            // listView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3});
-            this.dataGridView1.Location = new System.Drawing.Point(28, 87);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(341, 197);
-            this.dataGridView1.TabIndex = 8;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.No,
+            this.SubProceso,
+            this.Param1,
+            this.Param2,
+            this.Param3,
+            this.Param4});
+            this.listView1.Location = new System.Drawing.Point(28, 88);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(479, 197);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
-            // Column1
+            // No
             // 
-            this.Column1.HeaderText = "Column1";
-            this.Column1.Name = "Column1";
+            this.No.Text = "No";
             // 
-            // Column2
+            // SubProceso
             // 
-            this.Column2.HeaderText = "Column2";
-            this.Column2.Name = "Column2";
+            this.SubProceso.Text = "Sub Proceso";
+            this.SubProceso.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.SubProceso.Width = 99;
             // 
-            // Column3
+            // Param1
             // 
-            this.Column3.HeaderText = "Column3";
-            this.Column3.Name = "Column3";
+            this.Param1.Text = "Parametro 1";
+            this.Param1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Param1.Width = 78;
+            // 
+            // Param2
+            // 
+            this.Param2.Text = "Parametro 2";
+            this.Param2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Param2.Width = 78;
+            // 
+            // Param3
+            // 
+            this.Param3.Text = "Parametro 3";
+            this.Param3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Param3.Width = 79;
+            // 
+            // Param4
+            // 
+            this.Param4.Text = "Parametro 4";
+            this.Param4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Param4.Width = 81;
             // 
             // FormCrearProcesos2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 370);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(878, 494);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.txCodigoProceso);
             this.Controls.Add(this.lbCodigoProceso);
             this.Controls.Add(this.lbNombreProceso);
             this.Controls.Add(this.txNombreProceso);
             this.Name = "FormCrearProcesos2";
             this.Text = "FormCrearProcesos2";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,9 +148,12 @@
         private System.Windows.Forms.Label lbCodigoProceso;
         private System.Windows.Forms.Label lbNombreProceso;
         private System.Windows.Forms.TextBox txNombreProceso;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader No;
+        private System.Windows.Forms.ColumnHeader SubProceso;
+        private System.Windows.Forms.ColumnHeader Param1;
+        private System.Windows.Forms.ColumnHeader Param2;
+        private System.Windows.Forms.ColumnHeader Param3;
+        private System.Windows.Forms.ColumnHeader Param4;
     }
 }
